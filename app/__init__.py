17b501c71dbba6d2e9a5e87a39108e348284595f
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2023-02-18 20:51:26 +0000

    back end user login/register

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+from .extensions import bootstrap, db, moment
+from config import config
 
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
 
-from app import routes
+    bootstrap.init_app(app)
+    db.init_app(app)
+    moment.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
