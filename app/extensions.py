961dd24c2c50ee9d80072b888bdd8ad0915b6482
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2023-02-18 21:10:29 +0000

    login, register functions

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,9 @@
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_moment import Moment
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 moment = Moment()
+login_manager = LoginManager()
